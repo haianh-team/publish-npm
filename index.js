418b8c84d91f28e7036d85ea2f4c580b00cf4cd5
haianh123 <lehaianh111103@gmail.com>
2022-03-04 11:37:09 +0700

    add nhan chia tru

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
-module.exports = function sum(a,b){
+function sum(a,b){
     const tong = a+b
     console.log(`tong:${tong}`)
+}
+function tru(a,b){
+    const tong = a+b
+    console.log(`tong:${tong}`)
+}
+function nhan(a,b){
+    const tong = a+b
+    console.log(`tong:${tong}`)
+}
+function chia(a,b){
+    const tong = a+b
+    console.log(`tong:${tong}`)
+}
+module.exports = {
+    sum,
+    tru,
+    nhan,
+    chia
 }
